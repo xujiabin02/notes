3dc8fa9bc14a5779bcadb91ab320496e9e6de57d
xujiabin <xujiabin@growingio.com>
2021-04-07 09:52:21 +0800

    TOC

--- .NOTES/toc_build.sh
+++ .NOTES/toc_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
-> README.md
+> ../README.md
 for i in $(ls |grep -v "README.md"|grep -v ".assets"|grep -v toc_build.sh)
 do
 title=$(echo $i|awk -F"." '{print$1}')
-echo "###### [$title](./$i)" >> README.md
+echo "###### [$title](./$i)" >> ../README.md
 done
+cd ..
 git add .
 git commit -m "TOC"
 git push
