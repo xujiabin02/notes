380a12d5b90b1c45077bc14231b377b9e5496a57
xujiabin <xujiabin@xinluex.com>
2022-03-03 11:16:33 +0800

    toc change

--- .NOTES/toc_build.sh
+++ .NOTES/toc_build.sh
@@ -3,7 +3,7 @@
 for i in $(ls |grep -v "README.md"|grep -v ".assets"|grep -v toc_build.sh)
 do
 title=$(echo $i|awk -F"." '{print$1}')
-echo "###### [$title](.NOTES/$i)" >> ../README.md
+echo "- [$title](.NOTES/$i)" >> ../README.md
 done
 cd ..
 git add .
